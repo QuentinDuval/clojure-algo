ffb12e4009ce393cf7df1ea6dd6db961c716b76e
QuentinDuval <senmenty@gmail.com>
2016-05-14 17:34:26 +0200

    Playing with Ring

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+	     [org.clojure/clojure "1.8.0"]
+	     [javax.servlet/servlet-api "2.5"] ; Needed for "ring"
+	     [ring "1.4.0"] ; HTTP server
+	     [compojure "1.5.0"] ; HTTP rounting
+     ]
   :aot [clojure-algo.core clojure-algo.geometry]
   :main clojure-algo.core)
--- src/clojure_algo/core.clj
+++ src/clojure_algo/core.clj
@@ -1,7 +1,44 @@
 (ns clojure-algo.core
-  (:gen-class))
+  (:gen-class)
+  (:use [ring.adapter.jetty :only (run-jetty)])
+  (:use [ring.middleware.params :only (wrap-params)])
+  (:use [compojure.core :only (defroutes GET)])
+  (:require [compojure.route :as route]))
 
 (defn -main
   [& args]
   (println (+ 1 2)))
 
+
+
+
+; Playing with a small REST server
+
+(defn echo-handler
+  [{:keys [uri params]}]
+  {:body
+   (format "Path = \"%s\" with params \"%s\"" uri params)})
+
+(defn apply-fn
+  [name expr]
+  (let [result (eval (read-string expr))]
+    {:body
+     (str "[" name "] " expr " : " (str result))
+    }))
+
+; https://github.com/weavejester/compojure/wiki/Nesting-routes
+(defroutes app*
+  "Handler that routes requests to other handlers"
+  (GET "/" [] "<h1>Route page</h1>")
+  (GET "/echo/*" request echo-handler)
+  (GET "/fn/:name" [name expr] (apply-fn name expr))
+  (route/not-found "<h1>No match</h1>"))
+
+(def app
+  "Extract the parameters and forward to handler"
+  (wrap-params app*))
+
+;; Test code
+
+(def server (run-jetty #'app {:port 8080 :join? false}))
+(.stop server)
